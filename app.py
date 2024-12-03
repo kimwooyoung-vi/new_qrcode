37c0a67ae79e43940c3f8c6980cdf82787b29b69
kimwooyoung-vi <kwy@visioninside.co.kr>
2024-12-03 15:59:48 +0900

    코멘트 달아두기

--- app.py
+++ app.py
@@ -30,13 +30,13 @@ class PandasTableModel(QAbstractTableModel):
     def __init__(self, dataframe, parent=None):
         super().__init__(parent)
         self._dataframe = dataframe
-
+    # QAbstractTableModel의 메서드를 오버라이드
     def rowCount(self, parent=None):
         return len(self._dataframe)
-
+    # QAbstractTableModel의 메서드를 오버라이드
     def columnCount(self, parent=None):
         return len(self._dataframe.columns)
-
+    # QAbstractTableModel의 메서드를 오버라이드
     def data(self, index, role=Qt.ItemDataRole.DisplayRole):
         if not index.isValid():
             return None
@@ -46,7 +46,7 @@ class PandasTableModel(QAbstractTableModel):
                 return ""
             return str(value)
         return None
-
+    # QAbstractTableModel의 메서드를 오버라이드
     def headerData(self, section, orientation, role=Qt.ItemDataRole.DisplayRole):
         if role == Qt.ItemDataRole.DisplayRole:
             if orientation == Qt.Orientation.Horizontal:
@@ -56,77 +56,89 @@ class PandasTableModel(QAbstractTableModel):
         return None
 
 class MainWindow(QMainWindow):
+    # 메인 윈도우 클래스
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Attendance Help")
-        self.setWindowIcon(QIcon("./vision_logo.png"))
-        self.resize(800, 600)
-        self.central_widget = QWidget(self)
-        self.setCentralWidget(self.central_widget)
+        self.setWindowTitle("Attendance Help") # 창 제목
+        self.setWindowIcon(QIcon("./vision_logo.png")) # 창 아이콘 설정
+        self.resize(800, 600) # 창 크기 설정
+        self.central_widget = QWidget(self) # 중앙 위젯 설정
+        self.setCentralWidget(self.central_widget) # 중앙 위젯 지정
 
+        # 레이아웃 설정
         self.layout = QVBoxLayout(self.central_widget)
 
-        self.label = QLabel("Select Excel File")
+        self.label = QLabel("Select Excel File") # 파일선택 문구
         self.layout.addWidget(self.label)
 
+        # 파일 선택 버튼 설정
         self.selectFileBtn = QPushButton("Select Attendance File")
-        self.selectFileBtn.clicked.connect(self.select_file_dialog)
+        self.selectFileBtn.clicked.connect(self.select_file_dialog) # 클릭 시 엑셀 파일 선택 창 표시
         self.layout.addWidget(self.selectFileBtn)
 
+        # 엑셀 시트 선택 콤보박스
         self.sheet_combo = QComboBox()
-        self.sheet_combo.addItem("Select Sheet")  # Default item
-        self.sheet_combo.currentIndexChanged.connect(self.load_sheet_data)
+        self.sheet_combo.addItem("Select Sheet")  # 기본 항목
+        self.sheet_combo.currentIndexChanged.connect(self.load_sheet_data) # 선택한 엑셀파일의 시트목록 로드
         self.layout.addWidget(self.sheet_combo)
 
+        # 테이블 설정
         self.table_view = QTableView()
         self.table_view.setSizeAdjustPolicy(QTableView.SizeAdjustPolicy.AdjustToContents)
         self.table_view.setEditTriggers(QTableView.EditTrigger.NoEditTriggers)
-
         self.layout.addWidget(self.table_view)
 
+        # 출석 통계 결과 저장 버튼 설정 -> 누르면 몇차시 수업을 출석했는지 표기됨.
         self.saveResultsBtn = QPushButton("Save Results")
         self.saveResultsBtn.clicked.connect(self.save_results)
         self.layout.addWidget(self.saveResultsBtn)
 
+        # QR 코드 생성 버튼 설정
         self.qr_generate_btn = QPushButton("QR Generate")
         self.qr_generate_btn.clicked.connect(self.qr_generate_start)
-        self.qr_generate_btn.setEnabled(False)
+        self.qr_generate_btn.setEnabled(False) # 엑셀 시트 선택이전까지 비활성화
         self.layout.addWidget(self.qr_generate_btn)
 
+        # QR 코드 읽기 버튼 설정
         self.qr_btn = QPushButton("QR Reader")
         self.qr_btn.clicked.connect(self.qr_window)
-        self.qr_btn.setEnabled(False)
+        self.qr_btn.setEnabled(False) # 엑셀 시트 선택이전까지 비활성화
         self.layout.addWidget(self.qr_btn)
 
+        # 초기 값 설정
         self.file_path = None
         self.df = None
         self.sheet_names = []
         self.current_sheet= None
 
-        # Load saved settings
+        # 저장된 설정 로드 ( 마지막 선택한 엑셀 파일경로, 시트명 )
         self.load_settings()
         self.update_qr_button_state()
 
     def qr_window(self):
+        # QR 읽기 화면(창)을 띄움. -> qrReaderWidget.py에 정의됨.
         qr_reader_window = CameraViewer(self)
         qr_reader_window.qrProcessed.connect(self.handle_qr_processed)
         qr_reader_window.show()
         # pass
 
     def handle_qr_processed(self):
+        # QR 코드가 읽힐때마다 시트를 새로고침
         self.load_sheet_data()
         
     def qr_generate_start(self):
+        # QR 코드 생성시작
         path = Path(self.file_path).parent
         qr_folder_path = os.path.join(path,"qr_folder",self.current_sheet)
         if not os.path.exists(qr_folder_path):
-            os.makedirs(qr_folder_path)
+            os.makedirs(qr_folder_path) # 이미지를 저장할 폴더 생성
         for row_idx, row in self.df.iloc[1:].iterrows():
-            self.generateQR(row[1:3], qr_folder_path)
+            self.generateQR(row[1:3], qr_folder_path) # 차례대로 읽어 QR 코드 생성
         QMessageBox.information(self,"Information","QR Generate Completed",QMessageBox.StandardButton.Ok)
         
     def generateQR(self,data:pd.Series, qr_folder_path):
+        # qrcode 라이브러리를 사용하는 위치.
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -146,6 +158,7 @@ class MainWindow(QMainWindow):
         return file_path
 
     def select_file_dialog(self):
+        # 엑셀 파일 선택 창 호출
         file, _ = QFileDialog.getOpenFileName(self, 'Select File', "", "Excel Files (*.xlsx)")
         if file:
             file_without_extension = Path(file).stem
@@ -155,6 +168,7 @@ class MainWindow(QMainWindow):
             self.load_sheet_names()
 
     def load_sheet_names(self):
+        # 엑셀 파일에서 시트 목록을 불러옴 
         if self.file_path:
             try:
                 excel_file = pd.ExcelFile(self.file_path)
@@ -170,6 +184,7 @@ class MainWindow(QMainWindow):
                 print(f"Error loading sheet names: {e}")
 
     def load_sheet_data(self):
+        # 선택된 시트에서 데이터를 읽어옴
         if self.file_path:
             self.current_sheet = self.sheet_combo.currentText()
             if self.current_sheet != "Select Sheet" and self.current_sheet != "":
@@ -179,33 +194,32 @@ class MainWindow(QMainWindow):
                     
                 try:
                     self.df = pd.read_excel(self.file_path, sheet_name=self.current_sheet)
+                    # 읽어온 pandas 데이터를 QT 형식의 테이블로 변환
                     model = PandasTableModel(self.df)
-                    self.table_view.setModel(model)
-                    self.table_view.resizeColumnsToContents()
+                    self.table_view.setModel(model) # 테이블 뷰에 QAbstractTableModel 구현체 전달
+                    self.table_view.resizeColumnsToContents() # 열 너비 자동 조절
                     if "出席調査" in self.current_sheet:
-                        self.update_qr_button_state(False)
+                        self.update_qr_button_state(False) # 출석 조사가 포함된 시트명은 QR 버튼 비활성화
                     else:
-                        self.update_qr_button_state(True)
+                        self.update_qr_button_state(True) # 
                 except Exception as e:
                     print(f"Error loading sheet data: {e}")
 
     def add_date_column(self, sheet_name):
-        # try:
-            df = pd.read_excel(self.file_path, sheet_name = sheet_name)
-            if df['学年'][0] != '年月日':
-                # print(df['学年'][0])
-                # 이곳에 빈열 추가
-                empty = pd.DataFrame(index=range(0,1))
-                empty.loc[0,'学年'] = '年月日'
-                df = pd.concat([empty,df], axis=0)
-                with pd.ExcelWriter(self.file_path, mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
-                    df.to_excel(writer,sheet_name=sheet_name, index=False)
-
-
-        # except Exception as e:
-        #     print(e)
+        # 출석부 첫 행에 요일 정보 추가
+        df = pd.read_excel(self.file_path, sheet_name = sheet_name)
+        
+        if df['学年'][0] != '年月日':
+            # print(df['学年'][0])
+            # 이곳에 빈열 추가
+            empty = pd.DataFrame(index=range(0,1))
+            empty.loc[0,'学年'] = '年月日'
+            df = pd.concat([empty,df], axis=0)
+            with pd.ExcelWriter(self.file_path, mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
+                df.to_excel(writer,sheet_name=sheet_name, index=False)
 
     def update_qr_button_state(self, enabled=False):
+        # QR 버튼 상태 업데이트(활성/비활성) - 出席調査 가 들어가있으면 비활성화 TODO[출석부 형태의 규칙을 따르면 활성화되도록 변경]
         if enabled:
             self.qr_generate_btn.setEnabled(True)
             self.qr_generate_btn.setStyleSheet("background-color: #4CAF50; color: white;")  # Active style
@@ -219,6 +233,7 @@ class MainWindow(QMainWindow):
 
 
     def save_results(self):
+        # 결과를 저장하는 메서드
         if self.file_path:
             results = []
             today_date = datetime.now().strftime("%Y-%m-%d")
@@ -237,7 +252,7 @@ class MainWindow(QMainWindow):
                             grade = row[student_grade]
                             id = row[student_id]
                             name = row[student_name]
-                            count_x = row[attendance_columns].eq('x').sum()
+                            count_x = row[attendance_columns].eq('x').sum() # 결석 수 계산
                             results.append([sheet_name,grade, id,name, count_x, total_day])
                     except Exception as e:
                         print(f"Error processing sheet {sheet_name}: {e}")
@@ -249,11 +264,12 @@ class MainWindow(QMainWindow):
             with pd.ExcelWriter(self.file_path, mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
                 # writer.book = book  # 이미 로드한 워크북을 writer에 설정
                 result_df.to_excel(writer, sheet_name=f'出席調査_{today_date}', index=False)
-            self.load_sheet_names()
+            self.load_sheet_names() # 시트 목록 갱신 -> 콤보박스에 出席調査_{today_date} 추가
         else:
             print("No file selected.")
 
     def save_settings(self):
+        # 설정 저장하는 메서드 -> 마지막 선택한 파일 경로, 시트명 저장
         settings = {
             "file_path": self.file_path,
             "current_sheet": self.current_sheet
@@ -263,6 +279,7 @@ class MainWindow(QMainWindow):
 
 
     def load_settings(self):
+        # 설정을 불러오는 메서드
         if Path(SETTINGS_FILE).exists():
             with open(SETTINGS_FILE, "r") as f:
                 settings = json.load(f)
@@ -272,8 +289,9 @@ class MainWindow(QMainWindow):
                     self.label.setText(f"Selected File: {file_without_extension}")
                     self.load_sheet_names()
 
+# 테스트할때만 사용하는 부분
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    app.exec()
+    app = QApplication(sys.argv) # QT 객체 생성
+    window = MainWindow() # 메인 윈도우 객체 생성
+    window.show() # 윈도우 표시
+    app.exec() # 실행.
--- qrReaderWidget.py
+++ qrReaderWidget.py
@@ -12,9 +12,11 @@ import logging
 import pandas as pd
 import numpy as np
 from PIL import ImageFont, ImageDraw, Image
-date_format = r"^\(\d{6}\)$"
 
+
+# 락 상태 해결을 위해 스레드 사용
 class CameraThread(QThread):
+    # 프레임 캡처 시그널, 카메라 준비 시그널
     frameCaptured = pyqtSignal(np.ndarray)
     cameraReady = pyqtSignal()
     def __init__(self, parent=None):
@@ -26,32 +28,35 @@ class CameraThread(QThread):
         self.capture = cv2.VideoCapture(0)  # 카메라 장치 열기
         self.running = True
         
+        # 열릴때까지 대기
         while not self.capture.isOpened():
             cv2.waitKey(100)
         
+        # 카메라 준비완료 시그널
         self.cameraReady.emit()
 
         while self.running:
             ret, frame = self.capture.read()
             if ret:
-                # OpenCV BGR 포맷을 QImage의 RGB 포맷으로 변환
+                # 프레임 좌우 반전
                 frame = cv2.flip(frame, 1)
                 
-                # 신호를 통해 메인 스레드에 프레임 전달
+                # 메인 스레드로 프레임 전달
                 self.frameCaptured.emit(frame)
-
+        # 카메라 리소스 해제
         self.capture.release()
 
     def stop(self):
+        # 카메라 스레드 종료 요청
         self.running = False
         self.wait()
 
-# logging.basicConfig(level=logging.DEBUG)
+# 리소스 경로를 절대 경로로 반환
 def resource_path(relative_path):
     try:
-        base_path = sys._MEIPASS
+        base_path = sys._MEIPASS # PyInstaller 로 패키징된 경우 사용하는 base_path
     except AttributeError:
-        base_path = os.path.abspath('.')
+        base_path = os.path.abspath('.') # 개발 중에서 사용하는 base_path
     return os.path.join(base_path, relative_path)
 
 font_path = resource_path("meiryo.ttc")
@@ -60,66 +65,72 @@ loading_path = resource_path("loading.gif")
 sound_path = resource_path("beep.wav")
 
 logging.debug("font_path:", font_path)
+
+# 폰트 설정, 파일을 로드할 수 없으면 기본 폰트 사용
 try:
     font = ImageFont.truetype(font_path,25)
     message_font = ImageFont.truetype(font_path,15)
 except IOError:
     font = ImageFont.load_default()
 
-META_FILE = "metainfo.json"
-
 class CameraViewer(QDialog):
     qrProcessed = pyqtSignal()
     last_processed_time = 0
     processing_interval = 2
+
     def __init__(self, parent=None):
         super().__init__(parent)
         
+        # 윈도우 크기 및 타이틀 설정
         self.resize(600,600)
         self.current_sheet = self.parent().current_sheet
         self.file_path = self.parent().file_path
         self.setWindowTitle("Camera Viewer")
         self.setWindowIcon(QIcon(logo_path))
-        logging.debug("logo_path:", logo_path)
+        logging.debug("logo_path:", logo_path) # 아이콘 설정
+
+        # 현재 선택된 학생 정보를 초기화
         self.selected = None
         self.current_no = ""
         self.current_name = ""
 
+        # 레이아웃 설정
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-        self.isChanged = False
+        self.isChanged = False # 출석 변경 여부 확인 -> 변경사항확인용
 
+        # 시트 데이터 읽기(parent로 부터 받아옴)
         self.df_sheet = pd.read_excel(self.file_path, sheet_name=self.current_sheet)
         self.sheet_label = QLabel(self)
         self.sheet_label.setFont(QFont(font_path,15))
         self.sheet_label.setText(f"Current Class: {self.current_sheet}")
         layout.addWidget(self.sheet_label)
 
+        # QR인증시 보여지는 라벨 설정(QR 정보 출력)
         self.message_label = QLabel(self)
         self.message_label.setStyleSheet("color: green;")
         self.message_label.setFont(QFont(font_path,20))
         self.message_label.setText(" ")
         layout.addWidget(self.message_label)
 
+        # 몇 차시인지 선택하는 콤보박스
         self.select_time = QComboBox()
-        self.load_times()
+        self.load_times() # 
         self.select_time.currentIndexChanged.connect(self.update_times)
         layout.addWidget(self.select_time)
 
+        # 카메라 화면 및 로딩 화면을 스택하기 위한 레이아웃 ( 카메라 화면 앞에 로딩 이미지를 겹쳐 불러옴)
         self.stack_layout = QStackedLayout()
         self.stack_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        
-        # video_layout = QHBoxLayout()
-        # video_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
+    
         ## 카메라 프레임 표시
         self.video_label = QLabel(self)
         self.video_label.setMinimumSize(640,480)
         self.video_label.setStyleSheet("border: 2px solid black;")
         self.stack_layout.addWidget(self.video_label)
 
-         # Loading label
+        # 로딩 화면 표시
         self.loading_label = QLabel(self)
         self.movie = QMovie(loading_path, QByteArray(), self)
         self.movie.setCacheMode(QMovie.CacheMode.CacheAll)
@@ -129,41 +140,42 @@ class CameraViewer(QDialog):
         self.stack_layout.addWidget(self.loading_label)
         # self.loading_label.setVisible(False)
         self.movie.start()
-
         layout.addLayout(self.stack_layout)
-        
-        self.camera_thread = CameraThread(parent=self)
-        self.camera_thread.frameCaptured.connect(self.update_frame)
-        self.camera_thread.cameraReady.connect(self.on_camera_ready)
 
+        # 카메라 스레드 초기화 설정        
+        self.camera_thread = CameraThread(parent=self)
+        self.camera_thread.frameCaptured.connect(self.update_frame) # 카메라 프레임 시그널 처리
+        self.camera_thread.cameraReady.connect(self.on_camera_ready) # 카메라 준비완료 시그널 처리
+
+        # 종료 버튼
         close_btn = QPushButton("Close")
         close_btn.clicked.connect(self.close_camera)
         layout.addWidget(close_btn)
 
-        # sound Widget
+        # 소리 설정( 출석 체크 시 소리 재생 )
         self.sound = QSoundEffect()
         self.sound.setSource(QUrl.fromLocalFile(sound_path))
-        self.sound.setLoopCount(1)
-        
-        # self.sound.play()
-        # self.sound.setLoo
-        
+        self.sound.setLoopCount(1)    
 
-        # Create a QTimer to hide the message after the specified duration
+        # 일정 시간 후 QR 정보창을 숨김
         self.message_timer = QTimer()
         self.message_timer.setSingleShot(True)
         self.message_timer.timeout.connect(lambda: self.message_label.setText(" "))
 
 
     def show_loading(self, show=True):
+        # 로딩 화면 표시
         if show:
             self.stack_layout.setCurrentWidget(self.loading_label)
         else:
             self.stack_layout.setCurrentWidget(self.video_label)
+    
     def on_camera_ready(self):
+        # 카메라 준비 완료 시 로딩 화면 숨기기
         self.show_loading(False)
 
     def update_times(self):
+        # 시간 선택 변경 시
         self.selected = self.select_time.currentText().strip()
         self.df_sheet[self.selected] = self.df_sheet[self.selected].astype(str)
         if self.selected != "Select 回目":
@@ -172,15 +184,18 @@ class CameraViewer(QDialog):
             self.start_camera()
 
     def load_times(self):
+        # 시간 목록 로딩
         self.select_time.addItem("Select 回目")
         total_time = [col for col in self.df_sheet.columns if "回目" in col]
         self.select_time.addItems(total_time)
     
     def start_camera(self):
+        # 카메라 시작
         if not self.camera_thread.isRunning():
             self.camera_thread.start()
 
     def update_frame(self, q_img:np.ndarray):
+        # 카메라 프레임 업데이트
         frame = self.read_frame(q_img)
         rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         h, w, ch = rgb_image.shape
@@ -188,6 +203,7 @@ class CameraViewer(QDialog):
         self.video_label.setPixmap(QPixmap.fromImage(q_img))
 
     def read_frame(self, frame):
+        # 프레임에서 QR 코드 디코딩
         try:
             barcodes = decode(frame)
             for barcode in barcodes:
@@ -197,6 +213,7 @@ class CameraViewer(QDialog):
                 self.current_no = barcode_array[0] # 번호
                 self.current_name = barcode_array[1] # 이름
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
+                # QR 코드가 읽혔을 때 출석 처리
                 if (self.message_label.text() == " "):
                     self.updateAttendance()
             return frame
@@ -204,11 +221,13 @@ class CameraViewer(QDialog):
             print(e)
 
     def close_camera(self):
+        # 카메라 스레드 종료
         self.camera_thread.stop()
         self.camera_thread.quit()
         self.close()
 
     def closeEvent(self, event):
+        # 창 닫을 때 카메라 종료 및 출석체크 처리
         self.close_camera()
         super().closeEvent(event)
 
@@ -241,24 +260,26 @@ class CameraViewer(QDialog):
             return new_name
 
     def show_temporary_message(self, message, duration=3500):
+        # 출석 체크 메세지 표시 
         if self.message_label.isVisible():
-            # If a message is already visible, stop the current timer
+            # 메세지가 비활성화일때 타이머 중지
             self.message_timer.stop()
-
         self.message_label.setText(message)
         self.message_timer.start(duration)
 
     def updateAttendance(self):
+        # 출석 처리
         student_id = self.current_no
         student_name = self.current_name
 
         if not student_id or not student_name:
             return
         
-        # Ensure IDs and names are stripped of extra whitespace
+        # 학번, 이름 공백 제거
         self.df_sheet['学籍番号'] = self.df_sheet['学籍番号'].astype(str).str.strip()
         self.df_sheet['氏名'] = self.df_sheet['氏名'].astype(str).str.strip()
 
+        # 시트(데이터 프레임)에서 일치하는 학생 찾기
         student_row = self.df_sheet[
             (self.df_sheet['学籍番号'] == student_id) & (self.df_sheet['氏名'] == student_name)
         ]
@@ -268,6 +289,7 @@ class CameraViewer(QDialog):
             current_value = self.df_sheet.at[student_index, self.selected]
 
             if current_value != str('o'):
+                # 출석 처리
                 self.df_sheet.at[student_index, self.selected] = str('o')
                 # self.processed_students.add(student_id)
                 # self.qr_label.setText(f"{student_id} Attendance has been recorded.")
@@ -280,6 +302,7 @@ class CameraViewer(QDialog):
         else:
             self.show_temporary_message("Not existed in Attendance list, Please contact the administrator.")
             return
+        
         if self.isChanged == False:
             self.isChanged = True
     
